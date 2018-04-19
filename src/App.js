079347b651de68cd3477ca92d56b48c4a49307fb
Rogelio Molina <rogelio.molina1@gmail.com>
2018-04-18 20:02:25 -0700

    added submission 0.1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+export class App extends React.Component {
+handleSubmit(){
+  <h1>{this._input.value}</h1>
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src="./logo.png" className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to PickUP</h1>
+          <h1 className="App-title">The Hub for Connecting Players.</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3 className="App-newGame">
+          To setup a pickup game please enter the required information below.
+        </h3>
+
+      <section className="SubmissionHome">
+        <form
+         className="form-inline"
+         role="form"
+         onSubmit={this.handleSubmit}
+       >
+          <input
+            id="sport"
+            placeHolder="Activity"
+            size = "30"
+            ref={ref => (this._input = ref)}/>
+
+          <input
+              id="name"
+              placeHolder="Name"
+              size = "30"/>
+          <input
+                id="location"
+                placeHolder="Location"
+                size = "50"/>
+
+          <button type="submit" className=".App-submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <CurrentGames/>
       </div>
     );
   }
 }
 
-export default App;
+class CurrentGames extends React.Component{
+  render(){
+    return(
+      <h3 className="App-curentGames" margin>
+        Below are the currently scheduled games.
+      </h3>
+
+    );
+  }
+}
