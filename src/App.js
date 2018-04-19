d8dcc6cb1519803a91c95d7245669e44a92951df
Rogelio Molina <rogelio.molina1@gmail.com>
2018-04-18 21:36:02 -0700

    style update 0.1

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ handleSubmit(){
       <div className="App">
         <header className="App-header">
           <img src="./logo.png" className="App-logo" alt="logo" />
-          <h1 className="App-title">The Hub for Connecting Players.</h1>
+          <h1 className="App-title">The Hub for Connecting Players</h1>
         </header>
         <h3 className="App-newGame">
-          To setup a pickup game please enter the required information below.
+          To setup a pickup game please enter the required information below
         </h3>
 
       <section className="SubmissionHome">
@@ -24,26 +24,24 @@ handleSubmit(){
          onSubmit={this.handleSubmit}
        >
           <input
-            id="sport"
-            placeHolder="Activity"
-            size = "30"
-            ref={ref => (this._input = ref)}/>
+             type="text"
+             id="sport"
+             placeHolder="Activity"/>
 
           <input
-              id="name"
-              placeHolder="Name"
-              size = "30"/>
+             type="text"
+             id="name"
+             placeHolder="Name"/>
           <input
-                id="location"
-                placeHolder="Location"
-                size = "50"/>
-
-          <button type="submit" className=".App-submit">
-            Submit
-          </button>
+             type="text"
+             id="location"
+             placeHolder="Location"/>
+           <div>
+             <input type="submit" value="Submit"/>
+           </div>
         </form>
       </section>
-      <CurrentGames/>
+      <div>  <CurrentGames/> </div>
       </div>
     );
   }
@@ -53,7 +51,7 @@ class CurrentGames extends React.Component{
   render(){
     return(
       <h3 className="App-curentGames" margin>
-        Below are the currently scheduled games.
+        Below are the currently scheduled games
       </h3>
 
     );
