fa247a01ca01a9fa2fc71b4c6089b7d211cb31d6
Rogelio Molina <rogelio.molina1@gmail.com>
2018-04-19 18:55:35 -0700

    Frontend 1.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
+import {CurrentGames} from './CurrentGames';
 
-export let games=[];
 
 export class App extends React.Component {
   render() {
@@ -19,92 +19,6 @@ export class App extends React.Component {
 <CurrentGames games={this.props.games}/>
         </section>
       </div>
-    );
-  }
-}
-
-export class CurrentGames extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: '',
-      games: props.games
-    };
-  }
-
-updateSearch(event){
-  this.setState({search: event.target.value});
-}
-
-addGame(event) {
-  event.preventDefault();
-  let sport = this.refs.sport.value;
-  let name = this.refs.name.value;
-  let location = this.refs.location.value;
-  let id = Math.floor((Math.random()*100)+1);
-  this.setState({
-    games: this.state.games.concat({id, sport, name, location})
-  })
-  this.refs.sport.value='';
-  this.refs.name.value='';
-  this.refs.location.value='';
-}
-
-  render(){
-    let filteredGames = this.state.games.filter(
-      (game) => {
-        return ((game.sport.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1)||
-          (game.name.toLowerCase().indexOf(this.state.search.toLowerCase())!== -1)||
-          (game.location.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1));
-      }
-    );
-    return(
-      <div>
-
-        <form
-         className="form-inline"
-         onSubmit={this.addGame.bind(this)}
-       >
-            <input
-              type="text"
-              ref="sport"
-              placeholder="Activity"/>
-            <input
-              type="text"
-              ref="name"
-              placeholder="Name"/>
-           <input
-              type="text"
-              ref="location"
-              placeholder="Location"/>
-
-            <div className="App-submitButton">
-              <input type="submit" value="Submit"/>
-            </div>
-
-          </form>
-
-        <input type="text" placeholder="Search"
-          value={this.state.search}
-          onChange={this.updateSearch.bind(this)}/>
-
-
-      <ul>
-          {filteredGames.map((game)=>{
-            return <Game game = {game} key={game.id}/>
-          })}
-      </ul>
-    </div>
-    );
-  }
-}
-
-export class Game extends React.Component{
-  render(){
-    return(
-      <li>
-      {this.props.game.sport} {this.props.game.name} {this.props.game.location}
-          </li>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {App, games} from './App';
+import {App} from './App';
+import {games} from './CurrentGames';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App games={games}/>, document.getElementById('root'));
