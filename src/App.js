15124a70f9b5a0e68267b8638b845fb626946412
Rogelio Molina <rogelio.molina1@gmail.com>
2018-04-19 17:59:56 -0700

    Filter function enabled version 0.1

--- src/App.js
+++ src/App.js
@@ -1,59 +1,125 @@
 import React from 'react';
 import './App.css';
 
-export class App extends React.Component {
-handleSubmit(){
-  <h1>{this._input.value}</h1>
-}
+export let games = [{
+  id: 1,
+  sport: 'Soccer',
+  name: 'Rogelio',
+  location: 'Livermore'
+},{
+  id: 2,
+  sport: 'Basketball',
+  name: 'Tim',
+  location: 'Pleasanton'
+},{
+  id: 3,
+  sport: 'Surfing',
+  name: 'Brad',
+  location: 'Santa Cruz'
+}];
 
+export class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src="./logo.png" className="App-logo" alt="logo" />
+          <img src="/logo.png" className="App-logo" alt="logo" />
           <h1 className="App-title">The Hub for Connecting Players</h1>
         </header>
-        <h3 className="App-newGame">
+        <h1 className="App-newGame">
           To setup a pickup game please enter the required information below
-        </h3>
+        </h1>
 
       <section className="SubmissionHome">
-        <form
-         className="form-inline"
-         role="form"
-         onSubmit={this.handleSubmit}
-       >
-          <input
-             type="text"
-             id="sport"
-             placeHolder="Activity"/>
-
-          <input
-             type="text"
-             id="name"
-             placeHolder="Name"/>
-          <input
-             type="text"
-             id="location"
-             placeHolder="Location"/>
-           <div>
-             <input type="submit" value="Submit"/>
-           </div>
-        </form>
-      </section>
-      <div>  <CurrentGames/> </div>
+<CurrentGames games={this.props.games}/>
+        </section>
       </div>
     );
   }
 }
 
 class CurrentGames extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state={
+      search: '',
+      games: props.games
+    };
+  }
+
+updateSearch(event){
+  this.setState({search: event.target.value});
+}
+
+addGame(event) {
+  event.preventDefault();
+  let sport = this.refs.sport.value;
+  let name = this.refs.name.value;
+  let location = this.refs.location.value;
+  let id = Math.floor((Math.random()*100)+1);
+  this.setState({
+    games: this.state.games.concat({id, sport, name, location})
+  })
+  this.refs.sport.value='';
+  this.refs.name.value='';
+  this.refs.location.value='';
+}
+
+  render(){
+    let filteredGames = this.props.games.filter(
+      (game) => {
+        return ((game.sport.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1)||
+          (game.name.toLowerCase().indexOf(this.state.search.toLowerCase())!== -1)||
+          (game.location.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1));
+      }
+    );
+    return(
+      <div>
+
+        <form
+         className="form-inline"
+         onSubmit={this.addGame.bind(this)}
+       >
+            <input
+              type="text"
+              ref="sport"
+              placeholder="Activity"/>
+            <input
+              type="text"
+              ref="name"
+              placeholder="Name"/>
+           <input
+              type="text"
+              ref="location"
+              placeholder="Location"/>
+
+            <div className="App-submitButton">
+              <input type="submit" value="Submit"/>
+            </div>
+
+          </form>
+
+        <input type="text" placeholder="Search"
+          value={this.state.search}
+          onChange={this.updateSearch.bind(this)}/>
+
+
+      <ul>
+          {filteredGames.map((game)=>{
+            return <Game game = {game} key={game.id}/>
+          })}
+      </ul>
+    </div>
+    );
+  }
+}
+
+class Game extends React.Component{
   render(){
     return(
-      <h3 className="App-curentGames" margin>
-        Below are the currently scheduled games
-      </h3>
-
+      <li>
+      {this.props.game.sport} {this.props.game.name} {this.props.game.location}
+          </li>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
+import {games} from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App games={games}/>, document.getElementById('root'));
 registerServiceWorker();
