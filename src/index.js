89ad4abf0fa61c156f572dea9278a57d7ae15d4c
Rogelio Molina <rogelio.molina1@gmail.com>
2018-04-26 14:22:23 -0700

    Working Nav

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import {App} from './App';
-import {games} from './CurrentGames';
+import { Router, Route} from 'react-router';
+import { BrowserRouter } from 'react-router-dom'
+import './css/index.css';
+import NavBar from './components/NavBar';
+import App from './components/App';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import {games} from './components/CurrentGames';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App games={games}/>, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+    <Route path="/" component={NavBar}/>
+    <Route path="/app" render={()=><App games={games}/>}/>
+    <Route path="/signin" component={SignIn}/>
+    <Route path="/signup" component={SignUp}/>
+    </div>
+  </BrowserRouter>, document.getElementById('root')
+);
 registerServiceWorker();
